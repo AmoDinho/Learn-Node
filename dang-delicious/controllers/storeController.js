68475c983f383e31b2854b614ec5c42539784c3f
Amo Moloko <u14284783@tuks.co.za>
2018-06-14 15:08:03 +0200

    added Image Middleware

--- dang-delicious/controllers/storeController.js
+++ dang-delicious/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next){
+      const isPhoto = file.mimetype.startsWith('image/');
+      if(isPhoto){
+          next(null,true);
+      } else{
+          next({message:'That filetype is not allowed!'}, false);
+      }
+    }
+}
 
 exports.homePage = (req, res) =>{
     res.render('index');
@@ -9,6 +24,26 @@ exports.addStore = (req, res) =>{
     res.render('editStore', {title:'Add Store'});
 };
 
+//Image middlerware - Multer
+exports.upload = multer(multerOptions).single('photo');
+
+//resize function
+exports.resize = async (req, res, next) => {
+    //check if there is no new file to resize
+    if(!req.file){
+        next();//skip to next middleware
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+
+    //Now we resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // Once we have writtern to the file system we keep gooing.
+    next();
+};
 
 exports.createStore = async (req,res) =>{
    const store = await (new Store(req.body)).save();
@@ -31,6 +66,9 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) =>{
+    //set the location data to be a point
+    req.body.location.type = 'Point'; 
+
     //find store
     const store =  await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
         new: true, //return new store instead of old one
