33907cc419dbac6a359c7abe29b323e2b60008bb
Amo Moloko <u14284783@tuks.co.za>
2018-06-20 13:12:34 +0200

    Did advanced aggregations and built up auth.

--- dang-delicious/controllers/storeController.js
+++ dang-delicious/controllers/storeController.js
@@ -86,3 +86,14 @@ exports.getStoreBySlug = async (req, res, next) =>{
      if(!store) return next();
      res.render('store', {store, title: store.name});
 };
+
+exports.getStoresByTag = async (req, res) =>{
+   const tag = req.params.tag;
+   const tagQuery = tag || {$exists:true};
+
+   const tagsPromise =  Store.getTagsList();
+   const storesPromie = Store.find({tags:tagQuery});
+   const [tags,stores] = await Promise.all([tagsPromise, storesPromie]);
+   
+    res.render('tag',{tags, title:'Tags', tag,stores});
+};
